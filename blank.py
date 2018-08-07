e1ee484d08adae1d87873bee68ca7ab14248db35
sryeturu <sryeturu@iastate.edu>
2018-08-06 22:06:51 -0500

    testing place objects

--- blank.py
+++ blank.py
@@ -1,3 +1,4 @@
+import numpy as np
 
 class Blank:
     
@@ -5,7 +6,7 @@ class Blank:
         
         self.img = img
         
-        sef.top_left = top_left
+        self.top_left = top_left
         self.bot_right = bot_right
         
         self.min_row = top_left[0]
@@ -15,31 +16,46 @@ class Blank:
         self.max_col = bot_right[1]
         
         self.can_place = np.zeros(shape=img.shape)
-    
+        
             
-    def place_object(obj, top_left_obj):
+    def place_object(self, obj, top_left_obj):
+        """ tries to place an object(image) on the current blank canvas
+        
+            Parameters
+            ----------
+            obj : numpy array
+                    image object to be drawn on top of blank
+            
+            top_left_obj : tuple (row, col)
+                    the top left row and column of where you want the obj to be placed
+            
+            Returns
+            ----------
+            bool
+                wether the placement was succesful or not
+        """
         
         bot_right_obj = (top_left_obj[0]+obj.shape[0], top_left_obj[1]+obj.shape[1])
         
         if top_left_obj[0] < self.top_left[0] and top_left_obj[1] < self.top_left[1]:
-            return false
+            return False
         
         if bot_right_obj[0] > self.bot_right[0] and bot_right_obj[1] > self.bot_right[1]:
-            return false
+            return False
         
-        row1 = top_left_obj[0]
-        row2 = top_left_obj[0] + img.shape[0]
+        min_row = top_left_obj[0]
+        max_row = top_left_obj[0] + obj.shape[0]
         
-        col1 = top_left_obj[1]
-        col2 = top_left_obj[1] + img.shape[1]
+        min_col = top_left_obj[1]
+        max_col = top_left_obj[1] + obj.shape[1]
         
-        if np.max(self.can_place[row1:row2, col1:col2] == 1):
-            return false
+        if np.max(self.can_place[min_row:max_row, min_col:max_col] == 1):
+            return False
 
-        self.can_place[row1:row2, col1:col2] == 1
-        self.img[row1:row2, col1:col2] == obj
-        
-        return true
+        self.can_place[min_row:max_row, min_col:max_col]  = 1
+        self.img[min_row:max_row, min_col:max_col] = obj
+    
+        return True
         
                                   
         
--- image_utils.py
+++ image_utils.py
@@ -3,8 +3,13 @@ import numpy as np
 import cv2 as cv
 import os
 
+from blank import Blank
+
 def jpg_to_numpy(directory_path, gray=True, delete=False):
     
+    if directory_path[-1] != '/':
+        directory_path += '/'
+        
     for img_path in glob.glob(directory_path + '/' + '*.jpg'):
         
         img = cv.imread(img_path)
@@ -20,3 +25,46 @@ def jpg_to_numpy(directory_path, gray=True, delete=False):
 
 def adaptive_thresh(img, block_size=45, constant=10):
     return cv.adaptiveThreshold(img, 255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, block_size, constant) 
+
+
+def parse_blank_fields(filename):
+    
+    fields = {}
+    
+    with open(filename) as f:
+        for line in f:
+            line = line.split(':')
+            
+            field_name = line[0].strip()
+            
+            coordinates = line[1].split(',')
+            row = int(coordinates[0].strip())
+            col = int(coordinates[1].strip())
+            
+            fields[field_name] = row,col
+    
+    return fields['top_left'], fields['bot_right']
+
+
+def get_blanks(directory_path):
+    
+    if directory_path[-1] != '/':
+        directory_path += '/'
+        
+    blank_names = set()
+    
+    for file_name in glob.glob(directory_path + '/*'):
+        cur_blank_name = file_name.split('/')[1]
+        cur_blank_name = cur_blank_name.split('.')[0]
+        
+        blank_names.add(cur_blank_name)
+        
+    blanks = []
+    
+    for blank in blank_names:
+        img = np.load(directory_path + '/' + blank + '.npy')
+        top_left, bot_right = parse_blank_fields(directory_path + '/' + blank + '.txt')
+        
+        blanks.append(Blank(img, top_left, bot_right))
+        
+    return blanks
