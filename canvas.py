774d1a053f3944577058103459659a47d34a95a8
sryeturu <sandeep.r.yeturu@gmail.com>
2018-08-29 11:43:48 -0500

    finished adding rotation function

--- canvas.py
+++ canvas.py
@@ -1,6 +1,10 @@
 import numpy as np
 import glob
 import os
+import cv2 as cv
+
+from scipy import ndimage
+
 from image_utils import get_number_of_images
 from config import parse_cfg
 
@@ -55,6 +59,10 @@ class Canvas:
         # specify these are paper attributes
         self.top_left = top_left
         self.bot_right = bot_right        
+        self.bot_left = bot_right[0], top_left[1]
+        self.top_right = top_left[0], bot_right[1]    
+        
+        self.x_y_contours = np.array([i[::-1] for i in [self.top_left, self.top_right, self.bot_right, self.bot_left]])
         
         self.min_row, self.max_row, self.min_col, self.max_col = top_left[0], bot_right[0], top_left[1], bot_right[1]
 
@@ -63,6 +71,10 @@ class Canvas:
         
     
     def draw_on_background(self, obj, top_left_obj):
+        
+        bot_right_obj = (top_left_obj[0]+obj.shape[0], top_left_obj[1]+obj.shape[1])
+        bot_left_obj = bot_right_obj[0], top_left_obj[1]
+        top_right_obj = top_left_obj[0], bot_right_obj[1]    
         
         for row in range(obj.shape[0]):        
             if top_left_obj[0] + row >= self.img.shape[0]:
@@ -73,11 +85,23 @@ class Canvas:
                 if top_left_obj[1] + col >= self.img.shape[1]:
                     break
                 
-                if (self.min_row <= (top_left_obj[0] + row) <= self.max_row) and (self.min_col <= (top_left_obj[1] + col) <= self.max_col):
+                if cv.pointPolygonTest(self.x_y_contours, (top_left_obj[1] + col, top_left_obj[0] + row), False) > 0.0:
                     continue
 
 
                 self.img[top_left_obj[0] + row, top_left_obj[1] + col] = obj[row, col]
+                
+    def rotate(self, degrees):
+        rad = np.deg2rad(degrees)
+        rt = np.array([[np.cos(rad), np.sin(rad)], [-np.sin(rad), np.cos(rad)]])
+        
+        original_shape = self.img.shape
+        self.img = ndimage.rotate(self.img, angle=degrees, cval=255)
+        
+        nw = np.dot(rt, self.x_y_contours.T - np.array(original_shape)[[1,0]].reshape(2,-1)/2.0)
+        nw += np.array([self.img.shape[1]/2, self.img.shape[0]/2]).reshape(2,-1)
+        nw = nw.astype(np.int32)
+        self.x_y_contours = nw.T
 
     def draw_on_paper(self, obj, top_left_obj):
         """ tries to place an object(image) on the canvas paper
@@ -97,20 +121,18 @@ class Canvas:
         """
         
         bot_right_obj = (top_left_obj[0]+obj.shape[0], top_left_obj[1]+obj.shape[1])
+        bot_left_obj = bot_right_obj[0], top_left_obj[1]
+        top_right_obj = top_left_obj[0], bot_right_obj[1]    
+        
+        obj_corners = [top_left_obj, top_right_obj, bot_right_obj, bot_left_obj]
+
+        for corner in obj_corners:
+            if cv.pointPolygonTest(self.x_y_contours, (corner[1], corner[0]), False) < 0.0:
+                return False
+            
         min_row, max_row, min_col, max_col = top_left_obj[0], bot_right_obj[0], top_left_obj[1], bot_right_obj[1]   
 
-        if top_left_obj[0] < self.top_left[0]:
-            return False
-        if top_left_obj[1] < self.top_left[1]:
-            return False
-        if bot_right_obj[0] > self.bot_right[0]:
-            return False
-        if bot_right_obj[1] > self.bot_right[1]:
-            return False
-        
-        
         overlay = np.copy(obj)
-        
         
         if np.alltrue(self.img[min_row:max_row, min_col:max_col] == 255):
             self.img[min_row:max_row, min_col:max_col] = overlay
